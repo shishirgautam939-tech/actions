c76f8785ead44dd76d16855d3333cb206a0c2f70
Kishan06319 <kishankhatiwada44@gmail.com>
2026-02-16 11:30:01 +0545

    testclass

--- registration/tests.py
+++ registration/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from .utils import add
 
-# Create your tests here.
+class AddFunctionTestCase(TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        # self.assertEqual(add(2, 3), 7)  # This test will fail
