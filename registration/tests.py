70982bb10f98a10300d226e72d0f9738eee294cf
Kishan06319 <kishankhatiwada44@gmail.com>
2026-02-16 11:35:57 +0545

    test-form

--- registration/tests.py
+++ registration/tests.py
@@ -1,7 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
 from .utils import add
+
 
 class AddFunctionTestCase(TestCase):
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
         # self.assertEqual(add(2, 3), 7)  # This test will fail
+        
+class REgistrationFormTestCase(TestCase):
+    def test_registration_form(self):
+        response = self.client.get(reverse('registration:form'))
+        #response = self.client.get(reverse('/registration/form/'))
+        self.assertEqual(response.status_code, 200)
+        # Additional assertions can be added here to check the form content
