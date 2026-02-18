b0828cc47aa8852bc2ec5e50acd5647b87e0a872
shishirgautam939-tech <shishirgautam939@gmail.com>
2026-02-18 11:52:05 +0545

    first commit

--- registration/tests.py
+++ registration/tests.py
@@ -8,12 +8,11 @@ class AddFunctionTestCase(TestCase):
         self.assertEqual(add(2, 3), 5)
         # self.assertEqual(add(2, 3), 7)  # This test will fail
         
-class REgistrationFormTestCase(TestCase):
-    def test_registration_form(self):
-        response = self.client.get(reverse('registration:form'))
-        #response = self.client.get(reverse('/registration/form/'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Name')
-        self.assertContains(response, 'Email')
-        self.assertContains(response, 'Password')
-        # Additional assertions can be added here to check the form content
+class RegistrationFormPageTestCase(TestCase):
+    def test_registration_contains_input_text(self):
+        response=self.client.get(reverse('registration:form')
+)
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response,"Name")
+        self.assertContains(response,"Email")
+        self.assertContains(response,"Password")
